be10e7024eacabdf6bcdd239a56385a8ad559b43
madalena-gentil <gentilmadalena7@gmail.com>
2026-05-20 15:00:17 -0300

    finalizado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'fake-store-app',
+  appName: 'App',
   webDir: 'www'
 };
 
